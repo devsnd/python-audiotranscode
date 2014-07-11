2d3d38c0c7dc8b6688ec8099e37b7d51f5917451
twall <tomwallroth@gmail.com>
2014-07-11 19:35:08 +0200

    bumped version to 0.2

--- audiotranscode/__init__.py
+++ audiotranscode/__init__.py
@@ -22,7 +22,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 """
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 import subprocess
 import re
